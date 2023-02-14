9f61545e1a668749f84c549941032db35ffffb84
Marcel Wiget <mwiget@gmail.com>
2023-02-14 15:07:02 +0100

    move output into tgw-spoke.tf

--- sites/tgw-spoke.tf
+++ sites/tgw-spoke.tf
@@ -70,3 +70,16 @@ module "spoke22" {
   }
 }
 
+output "spoke11" {
+  value = module.spoke11
+}
+output "spoke12" {
+  value = module.spoke12
+}
+output "spoke21" {
+  value = module.spoke21
+}
+output "spoke22" {
+  value = module.spoke22
+}
+
